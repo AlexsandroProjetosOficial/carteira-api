fb69b54a471025c9bb71826c40dfcaf8a6e93d15
Alexsandro Euzebio da Silva <alexsandro.euzebio@capoviagens.com.br>
2022-05-13 16:17:18 -0300

    Included documentatio with swagger.

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,18 +1,21 @@
-import { config } from 'dotenv';
-config();
 import 'reflect-metadata';
+import 'dotenv/config';
 import 'express-async-errors';
 import '@infra/shared/container'
-import express from 'express';
 import cors from 'cors';
-import { routes } from './routes';
+import express from 'express';
 import { ensureError } from '@middleware/ensureError';
+import { routes } from './routes';
+
+import swaggerFile from '../swagger.json';
+import swaggerUI from 'swagger-ui-express';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use('/api/v1', routes);
 app.use(ensureError);
 
