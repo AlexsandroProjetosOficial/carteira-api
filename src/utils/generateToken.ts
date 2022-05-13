b43c0ccf0eb8d111abd2b4052a1d3f7744860a0a
Alexsandro Euzebio da Silva <alexsandro.euzebio@capoviagens.com.br>
2022-05-13 16:25:38 -0300

    Change cert to private key.

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,15 +1,11 @@
-import path from "path";
-import { readFileSync } from "fs";
 import { sign } from "jsonwebtoken";
 import { IGenerateTokenDTO } from "./dtos/IGenerateTokenDTO";
 
 const generateToken = async ({ id, virtualAccountId }: IGenerateTokenDTO): Promise<string> => {
-	const privateKey = readFileSync(path.join(__dirname, '..', '..', 'src/configs/certs/jwtRS256.key'));
-
 	const token = sign({
 		id,
 		virtualAccountId
-	}, privateKey, { expiresIn: '8h', algorithm: 'RS256'});
+	}, process.env.PRIVATE_KEY, { expiresIn: '8h' });
 
 	return token;
 };
