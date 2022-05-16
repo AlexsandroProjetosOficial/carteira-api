58dc8443607731da4cc5ec605fac16861420053f
Alexsandro Euzebio da Silva <alexsandro.euzebio@capoviagens.com.br>
2022-05-16 19:21:13 -0300

    Expires token in 4 hours.

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -2,7 +2,7 @@ import { sign } from "jsonwebtoken";
 import { IGenerateTokenDTO } from "./dtos/IGenerateTokenDTO";
 
 const generateToken = async ({ id, virtualAccountId }: IGenerateTokenDTO): Promise<string> => {
-	return sign({ id, virtualAccountId }, process.env.PRIVATE_KEY, { expiresIn: '1m' });
+	return sign({ id, virtualAccountId }, process.env.PRIVATE_KEY, { expiresIn: '4h' });
 };
 
 export { generateToken };
